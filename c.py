a0c8287de9d0762f2bdb7126c85df1600e746945
Adam Kamarul Bahrin <79248873+adamhkb@users.noreply.github.com>
2021-07-08 11:38:28 +0800

    Update c.py

--- c.py
+++ c.py
@@ -4,7 +4,6 @@ import mysql.connector as mysql
 import json
 from obs import *
 
-
 ##### OBS Huawei
 AK = 'MKS9H7VLE3B556QLIMDB'
 SK = 'GUvX5vSrECTayt0IKQHWdYeU2DYFKWdeoDpFoLp9'
@@ -12,30 +11,29 @@ SERVER = 'https://obs.ap-southeast-3.myhuaweicloud.com'
 BUCKET = 'smart-access'
 
 obsClient = ObsClient(access_key_id = AK,
-    secret_access_key = SK,
-    server = SERVER)
+                      secret_access_key = SK,
+                      server = SERVER)
 
-def generate_presigned_url(OBS, client_method, expires_in):
+MYDB = mysql.connect(
+    host = '114.119.183.27',
+    user = 'root',
+    password = 'SmartAccess!',
+    database = 'smart_access_db'
+)
+
+def generate_presigned_url(mydb, client_method, expires_in):
     # Constructs a obs client instance with your account for accessing OBS
-    ###### DOC IMAGE PART
-    mydb = mysql.connect(
-        host = '114.119.183.27',
-        user = 'root',
-        password = 'SmartAccess!',
-        database = 'smart_access_db'
-    )
     cursor = mydb.cursor(buffered=True)
     cursor.execute('SELECT doc_image_path FROM users')
     for path in cursor:
         url = obsClient.createSignedUrl( # ObsClient.createSignedUrl
             method=client_method, # Method = GET
-            bucketName = 'smart-access',
+            bucketName = BUCKET,
             objectKey = path[0],
-            # BucketName = smart-access / ObjectKet = path[0] / expires = 2592000
+            # BucketName = smart-access / ObjectKey = path[0] / expires = 2592000
             expires = expires_in
         )
-        # print(url)
-        URL1.append(url)
+        URL1.append(url.signedUrl)
 
     dict1={}
     for i in range(0,len(URL1)):
@@ -48,32 +46,21 @@ def generate_presigned_url(OBS, client_method, expires_in):
     myfile1 = open('data.json', 'w+')
 
     # create your dict
-
     json.dump(dict1, myfile1)
     myfile1.close()
     mydb.commit()
 
-
-
     ###### SELFIE IMAGE PART
-
-    mydb = mysql.connect(
-        host = '114.119.183.27',
-        user = 'root',
-        password = 'SmartAccess!',
-        database = 'smart_access_db'
-    )
     cursor1 = mydb.cursor(buffered=True)
     cursor1.execute('SELECT selfie_image_path FROM users')
     for path in cursor1:
-        url3 = obsClient.createSignedUrl( # ObsClient.createSignedUrl
-            method=client_method, # Method = GET
-            bucketName = 'smart-access',
+        url3 = obsClient.createSignedUrl(
+            method=client_method,
+            bucketName = BUCKET,
             objectKey = path[0],
-            # BucketName = smart-access / ObjectKet = path[0] / expires_in = 2592000
-            expires = 2592000
+            expires = expires_in
         )
-        URL2.append(url3)
+        URL2.append(url3.signedUrl)
 
     dict2 = {}
     for i in range(0, len(URL2)):
@@ -90,9 +77,6 @@ def generate_presigned_url(OBS, client_method, expires_in):
     json.dump(dict2, myfile)
     myfile.close()
     mydb.commit()
-
-
-
 ######## End
 
 while True:
@@ -101,8 +85,6 @@ while True:
     URL2 = []
     # logger = logging.getLogger(__name__)
     METHOD = 'GET'
-    EXPIRES_IN = 2592000
-    generate_presigned_url(OBS = obsClient,
-                           client_method = METHOD,
-                           expires_in = EXPIRES_IN)
+    EXPIRES_IN = 30
+    generate_presigned_url(mydb = MYDB, client_method = METHOD, expires_in = EXPIRES_IN)
     time.sleep(EXPIRES_IN)
